33727c0bf3b4a074b97adddc7d2b1b89f3e59c5f
Luan Oliveira <luuan.dev@gmail.com>
2025-10-26 22:39:02 -0300

    remover codigo antigo

--- Api.Application/Mappers/GameMapper.cs
+++ Api.Application/Mappers/GameMapper.cs
@@ -1,43 +1,4 @@
-﻿//using Api.Application.Dtos;
-//using Api.Domain.Entitys;
-//using AutoMapper;
-
-//namespace Api.Application.Mappers
-//{
-//    public class GameProfile : Profile
-//    {
-//        public GameProfile()
-//        {
-//            // Request DTO -> GameRecommendation
-//            CreateMap<GameRecommendationRequestDto, GameRecommendation>()
-//                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre))
-//                .ForMember(dest => dest.Platform, opt => opt.MapFrom(src => src.Platform ?? "all"))
-//                .ForMember(dest => dest.AvailableRAM, opt => opt.MapFrom(src => src.AvailableRAM))
-//                .ForMember(dest => dest.RecommendedAt, opt => opt.Ignore())
-//                .ForMember(dest => dest.Id, opt => opt.Ignore())
-//                .ForMember(dest => dest.Title, opt => opt.Ignore());
-
-//            // ExternalGame -> GameRecommendation (para salvar no banco)
-//            CreateMap<ExternalGame, GameRecommendation>()
-//                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-//                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre))
-//                .ForMember(dest => dest.Platform, opt => opt.MapFrom(src => src.Platform))
-//                .ForMember(dest => dest.RecommendedAt, opt => opt.MapFrom(_ => DateTime.UtcNow))
-//                .ForMember(dest => dest.Id, opt => opt.Ignore())
-//                .ForMember(dest => dest.AvailableRAM, opt => opt.Ignore());
-
-//            // ExternalGame -> Response DTO
-//            CreateMap<ExternalGame, GameRecommendationResponseDto>()
-//                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-//                .ForMember(dest => dest.Link, opt => opt.MapFrom(src => src.GameUrl))
-//                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre))
-//                .ForMember(dest => dest.Platform, opt => opt.MapFrom(src => src.Platform));
-//        }
-//    }
-//}
-
-// Api.Application/Mappers/GameMapper.cs
-using Api.Application.Dtos;
+﻿using Api.Application.Dtos;
 using Api.Domain.Entitys;
 
 namespace Api.Application.Mappers
